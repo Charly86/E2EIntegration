825eaaa192ff7292e640105a01348dacaf813b70
CVillar <CVillar@SBS-CVILLAR.Itteria.cat>
2019-01-24 11:11:31 +0100

    Canvis en la classe TC001

--- E2ESeleniumJenkins/src/testcases/TC001.java
+++ E2ESeleniumJenkins/src/testcases/TC001.java
@@ -1,24 +1,73 @@
 package testcases;
 
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import static org.testng.Assert.fail;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 public class TC001 {
 	
-	@Test
-	public void testCase1() {
-		
+	private ChromeDriver driver;
+	private String baseUrl;
+	private boolean acceptNextAlert = true;
+	private StringBuffer verificationErrors = new StringBuffer();
+	
+	 
+	@BeforeClass
+	public void setUp() {
+	
 		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		 driver = new ChromeDriver();
+				
+	     baseUrl = "https://preproduccio.ioc.xtec.cat/campus/my/";
+			    
+	     
+		driver.manage().timeouts().implicitlyWait(35, TimeUnit.SECONDS);
+			  
+		driver.get(baseUrl);
 		
-		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+	
 		
-		driver.get("http://www.facebook.com");
-		driver.findElementById("email").sendKeys("Hello");
-		driver.findElementById("pass").sendKeys("Hello");
-		driver.quit();
+	
+	    }
+
+      
+	  @Test( priority = 1 )
+	  public void login() throws InterruptedException {
+		  
+		Thread.sleep(3000);
+		driver.findElement(By.id("login-campus-large")).click();
 		
-		
-	}
+		/*
+	    driver.findElement(By.id("username")).clear();
+	    driver.findElement(By.id("username")).sendKeys("usitteria");
+	    driver.findElement(By.id("password")).clear();
+	    driver.findElement(By.id("password")).sendKeys("%Ue5^wjaUXpx3UJT");
+	    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Cercar ...'])[1]/following::span[2]")).click();
+	    driver.findElement(By.id("submitbutton")).click();
+	    driver.findElement(By.id("label_3_23")).click();
+	    
+	    */
+	  }
+
+	 
+
+	  
+	  
+	  
+	
 
 }
